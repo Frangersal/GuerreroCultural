8b875d02dc19e2f78ecdcd26b62fce72b4a2e332
frangersal <frangersal@outlook.com>
2025-08-15 21:43:22 -0600

    4to intento: Edite el archivo start.sh para intentar desplegar en Railway

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-# Esperar a que la base de datos esté lista
-until curl -s $DB_URL >/dev/null; do
-    echo "La base de datos no está lista. Esperando 5 segundos..."
-    sleep 5
+echo "Esperando a que la base de datos esté lista..."
+
+until php artisan migrate:status > /dev/null 2>&1; do
+  echo "La base de datos no está lista. Esperando 5 segundos..."
+  sleep 5
 done
 
-echo "¡La base de datos está lista! Ejecutando migraciones..."
+echo "¡La base de datos está lista! Procediendo..."
 php artisan migrate --force
 
 echo "Iniciando el servidor..."

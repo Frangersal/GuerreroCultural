886a67c0c04f5c423a0fb7707ed6b7eb82686295
frangersal <frangersal@outlook.com>
2025-08-15 23:45:41 -0600

    19vo intento: Modifique start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ set -e
 
 echo "Esperando a que la base de datos esté lista..."
 
-# Pasa las variables de entorno de la base de datos directamente al script de PHP
-until php db_check.php "$DB_HOST" "$DB_PORT" "$DB_DATABASE" "$DB_USERNAME" "$DB_PASSWORD"; do
+# Usa un comando nc más robusto para verificar la conexión al puerto
+while ! nc -w 1 $DB_HOST $DB_PORT < /dev/null; do
     echo "La base de datos no está lista. Esperando 5 segundos..."
     sleep 5
 done
